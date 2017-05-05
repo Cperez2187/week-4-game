0dda647feef2fd8b8a00020fc130c21d44271ad6
cperez2187 <cperez2187@gmail.com>
2017-05-05 13:51:04 -0700

    updates to all

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	$(".crawl").hide(400);
+
+	//**************************
+	//	Character Objects
+	//**************************
+	var kenobi {
+		name: "Obi-Wan Kenobi",
+		hp: 150,
+		attack: 15,
+		defendAttack: 25,
+	}
+
+	var vader {
+		name: "Darth Julius Vader",
+		hp: 200,
+		attack: 20,
+		defendAttack: 40,
+	}
+
+	var kylo {
+		name: "Kylo Ren",
+		hp: 125,
+		attack: 15,
+		defendAttack: 30,
+	}
+
+	var luke {
+		name: "Luke Skywalker",
+		hp: 140,
+		attack: 12,
+		defendAttack: 35,
+	}
+})
